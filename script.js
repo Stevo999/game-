acbc3974c141bbee239d2e2ecb4321d35f583525
Steevo999 <gitaustephen84@gmail.com>
2023-07-25 05:52:57 +0300

    first commit

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 let move_speed = 3, grativy = 0.5;
-let bird_dy = 0; // Declare the bird_dy variable
 let bird = document.querySelector('.bird');
 let img = document.getElementById('bird-1');
-let sound_pointsound = new Audio('sounds effect/pointsound.mp3');
-let sound_diesound = new Audio('sounds effect/diesound.mp3');
+let sound_point = new Audio('sounds effect/point.mp3');
+let sound_die = new Audio('sounds effect/die.mp3');
 
 // getting bird element properties
 let bird_props = bird.getBoundingClientRect();
@@ -25,7 +24,7 @@ document.addEventListener('touchstart', handleTouch);
 document.addEventListener('touchend', handleTouchEnd);
 
 function handleKeyPress(e) {
-    if ((e.key === 'Enter' || e.type === 'touchstart') && game_state === 'Start') {
+    if (e.key == 'Enter' && game_state != 'Play') {
         document.querySelectorAll('.pipe_sprite').forEach((e) => {
             e.remove();
         });
@@ -40,7 +39,6 @@ function handleKeyPress(e) {
     }
 }
 
-
 function handleKeyUp(e) {
     if (e.key == 'ArrowUp' || e.key == ' ') {
         img.src = 'images/Bird.png';
@@ -48,24 +46,17 @@ function handleKeyUp(e) {
 }
 
 function handleTouch(e) {
-    e.preventDefault(); // Prevent default touch behavior (e.g., scrolling)
-    if (game_state === 'Start') {
+    if (game_state === 'Play') {
         img.src = 'images/Bird-2.png';
         bird_dy = -7.6;
-        game_state = 'Play'; // Start the game on touch
-        message.innerHTML = ''; // Remove the starting message
-        message.classList.remove('messageStyle');
-        play(); // Start the game loop
     }
 }
 
 function handleTouchEnd(e) {
-    e.preventDefault(); // Prevent default touch behavior (e.g., scrolling)
     if (game_state === 'Play') {
         img.src = 'images/Bird.png';
     }
 }
-
 
 function play(){
     function move(){
@@ -84,12 +75,12 @@ function play(){
                     message.innerHTML = 'Game Over'.fontcolor('red') + '<br>Press Enter To Restart';
                     message.classList.add('messageStyle');
                     img.style.display = 'none';
-                    sound_diesound.play();
+                    sound_die.play();
                     return;
                 }else{
                     if(pipe_sprite_props.right < bird_props.left && pipe_sprite_props.right + move_speed >= bird_props.left && element.increase_score == '1'){
                         score_val.innerHTML =+ score_val.innerHTML + 1;
-                        sound_pointsound.play();
+                        sound_point.play();
                     }
                     element.style.left = pipe_sprite_props.left - move_speed + 'px';
                 }
